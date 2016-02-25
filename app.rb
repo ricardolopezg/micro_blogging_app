0415922aa6ed9db989cc07f383be484004c1958a
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-02-25 17:53:23 -0500

    updates 1

--- app.rb
+++ app.rb
@@ -12,3 +12,19 @@ get "/" do
 
 	erb :home
 end
+
+get "/profile" do 
+  @posts = Post.all
+  
+  erb :profile
+end
+
+post "/profile" do 
+  Post.create(user_id: user.id, content: [params:content], post_date: Time.now)
+  redirect "/profile"
+end
+
+get "/feed" do 
+
+  erb :feed
+end
