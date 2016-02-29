0030c401edd810ff8907fb714c90c3b30c1943d2
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-02-29 16:44:23 -0500

    Posts made editable

---
+++ public/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $(".edit_post_btn").on("click", function(e) {
+    $(e.target).next('.update_post_form').show();
+  })
+
+  $("#cancel_post_edit").on("click", function(e) {
+    $(e.target).next('.update_post_form').hide();
+  })
+
+
+
+
+}); // END DOC READY
